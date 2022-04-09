f77434f90a407df021146e7011435a4e4a848f41
Aleksey <Alik29247@gmail.com>
2022-04-09 19:25:10 +0700

    initial commit

---
+++ src/test/java/ru.netology/DeliveryTest.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class DeliveryTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+}
